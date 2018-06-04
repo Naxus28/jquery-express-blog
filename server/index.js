1fe4733ef807bd11b11c20387b4bb0c475208df2
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-06-04 17:16:22 -0400

    minor edit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import blogRoutes from './api/blog/blogRoutes';
 const app = express();
 const PORT = process.env.PORT || 8080;
 const URL = process.env.NODE_ENV === 'production'
-              ? `https://frozen-shore-58330.herokuapp.com/${PORT}`
+              ? `https://frozen-shore-58330.herokuapp.com`
               : `http://localhost:${PORT}`;
 
 appMiddleware(app, express);
