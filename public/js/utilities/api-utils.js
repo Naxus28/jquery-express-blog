d79f8ee6533aedc0bb1df0e40313488c72959371
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-06-11 16:23:03 -0400

    adds api error route, controller, and regex--styles the template as well

--- public/js/utilities/api-utils.js
+++ public/js/utilities/api-utils.js
@@ -8,13 +8,44 @@ const handleApiError = (err, context = false) => {
     ? err
     : `${err.status} ${err.responseJSON.message}`;
 
-  context.app.swap('');
 
-  context
-    .render('../../templates/error.template', { error })
-    .appendTo(context.$element())
-    .then(content =>  {
-      $('.error').hide();
-      $('.error').show()
+  context.redirect(`#/error/${error}`);  
+  // context.app.swap('');
+
+  // context
+  //   .render('../../templates/error.template', { error })
+  //   .appendTo(context.$element())
+  //   .then(content =>  {
+  //     $('.error').hide();
+  //     $('.error').show()
+  //   });
+};
+
+
+/**
+ * AJAX DELETE 
+ * @return {undefined}
+ */
+const deletePost = () => {
+  $('body').on('click', '.delete-post', e => {
+    if (!confirm('This action cannot be undone. Do you want to proceed?')) {
+      return;
+    }
+
+    const $postParent = $(e.target).closest('.blog-posts__post');
+    const textFields = getBlogTextFields(e);
+
+     // DELETE
+    $.ajax({
+      url: `${BLOG_ENDPOINT}\/${textFields.id}`,
+      method: 'DELETE',
+      dataType: 'json',
+      headers: {
+        'Content-Type': 'application/json' // browsers default Content-Type to application/x-www-form-urlencoded
+      },
+      data: JSON.stringify(textFields), // need to stringify to send as json
+      success: () => $postParent.remove(), // remove the blog node
+      error: handleApiError
     });
+  });
 };
