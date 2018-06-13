c01c67924e6f6fafe9e69420914e56bdb935bf7e
naxus28 <gabrielferraz27@gmail.com>
2018-06-12 23:14:14 -0400

    sets prod env on packge.json; cleans up config file

--- server/dev/config/production.js
+++ server/dev/config/production.js
@@ -1,7 +1,7 @@
 export default {
   logging: true,
   db: {
-    url: process.env.MONGOLAB_URI // check heroku env variables
+    url: 'mongodb://heroku_vv4c70kq:8fpfipni62f34ksco761jmlrvo@ds257640.mlab.com:57640/heroku_vv4c70kq' // check heroku env variables
   },
   url: 'https://frozen-shore-58330.herokuapp.com'
 };
