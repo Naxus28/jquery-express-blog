0d615a767059665cd4925024c6dd7625ea697908
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-06-19 10:38:01 -0400

    edits comments on test file

--- server/dev/api/login/loginRoutes.js
+++ server/dev/api/login/loginRoutes.js
@@ -3,7 +3,7 @@ import loginCtrl from './loginCtrl';
 import { localAuth } from '../auth/authenticate';
 
 // if request is authenticed via 'localAuth' middleware (using passport-local strategy)
-// the control flow is passed to the controller where the jwt is created via a helper method and
+// the control flow is passed to the controller, where the jwt is created via a helper method and
 // sent back to the client
 router.route('/')
   .post(localAuth, loginCtrl);
