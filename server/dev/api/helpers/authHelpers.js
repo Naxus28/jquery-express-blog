e387ea60406072e15d0e226f729f0dab20aa32fc
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-06-19 10:48:32 -0400

    moves auth helper to /helpers

---
+++ server/dev/api/helpers/authHelpers.js
@@ -0,0 +1,25 @@
+import { sign as jwtSign } from 'jsonwebtoken';
+import config from '../../config/config';
+
+/**
+ * this function creates the jwt 
+ * using https://github.com/auth0/node-jsonwebtoken
+ * @param  {Object} user  
+ * @return {jwt} 
+ */
+const createJwt = userEmail => {
+  // see options at https://github.com/auth0/node-jsonwebtoken#token-expiration-exp-claim
+  // 'iat' is included by default
+  const jwtOptions = {
+    subject: userEmail,
+    expiresIn: config.jwtExpiry,
+    algorithm: config.jwtAlgorithm
+  };
+
+  return jwtSign({ userEmail }, config.jwtSecret, jwtOptions);
+};
+
+
+export {
+  createJwt
+};
