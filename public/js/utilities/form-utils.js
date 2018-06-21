1d19587536584fca6db7c18d8e3b838fe983aad5
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-06-21 17:19:58 -0400

    moves hidden input outside flex div to preserve flex display on update post form

--- public/js/utilities/form-utils.js
+++ public/js/utilities/form-utils.js
@@ -11,8 +11,8 @@ const getUpdatePostForm = jqueryEvent => {
     <div class="update-form">
       <div class="update-error error"></div>
       <form action="#/" method="POST" class="blog-update" onsubmit="return false;" id=${id}>
+        <input type="hidden" name="author" value="${authorId}">
         <div class="input-wrapper">
-          <input type="hidden" name="author" value="${authorId}">
           <input class="update-form__author" name="authorEmail" placeholder="Author" value="${author}" readonly>
           <input name="title" placeholder="Title" value="${title}">
         </div>
