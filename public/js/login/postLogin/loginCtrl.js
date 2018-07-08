f39a2e60513bd5d6ac8b65cb42ce83c3f04f5765
naxus28 <gabrielferraz27@gmail.com>
2018-07-08 12:48:32 -0400

    implements login post route

---
+++ public/js/login/postLogin/loginCtrl.js
@@ -0,0 +1,47 @@
+/**
+* AJAX POST 
+* 'context' is passed from sammy route to which this function is tied
+* @return {undefined}
+*/
+const doLogin = context => {
+  let $form = $('.login__form'),
+      $error = $('.login__error'),
+      data = $form.serializeObject();
+      incompleteFields = fieldsIncomplete(data, 'login');
+
+   if (incompleteFields.length) {
+    incompleteFields = incompleteFields.length === 1
+        ? incompleteFields
+        : formatIncompleteFieldsErrorMsg(incompleteFields);
+
+    $error.text('');
+    $error.text(`Please enter ${incompleteFields} before submitting the form.`);
+    $error.show();
+
+    return;
+   }
+
+   // POST
+  $.ajax({
+    url: LOGIN_ENDPOINT,
+    method: 'POST',
+    dataType: 'json',
+    headers: {
+      'Content-Type': 'application/json' // browsers default Content-Type to application/x-www-form-urlencoded
+    },
+    data: JSON.stringify(data), // need to stringify to send as json
+    success: res => {
+
+    	console.log('res: ', res);
+			      
+      // interpolate the variables on the template
+      // and append to '.blog-posts' in submit-form.template
+      // previously loaded into the context 
+      // context
+      //   .render('../../templates/partials/posts.template', { post: updatedPost })
+      //   .prependTo('.blog-posts');
+
+    },
+    error: err => handleApiError(err, context)
+  });
+};
