e94ffb1bcce265c17d89bb790220dd997ea4676b
naxus28 <gabrielferraz27@gmail.com>
2018-07-07 20:55:52 -0400

    adds color scheme and styles some components

--- public/js/home/homeCtrl.js
+++ public/js/home/homeCtrl.js
@@ -6,39 +6,46 @@ const getPosts = context => {
   // http://sammyjs.org/docs/api/0.7.4/all#Sammy.Application-swap
   context.app.swap('');
 
-  $.ajax({
-    url: BLOG_ENDPOINT,
-    dataType: 'json',
-    success: posts => {
+  // render top bar, header, and footer (prepend header first then topbar--topbar will be on top)
+  context.render('../../templates/ui/header.template').prependTo(context.$element());
+  context.render('../../templates/ui/top-bar.template').prependTo(context.$element());
+  context.render('../../templates/ui/footer.template').appendTo(context.$element());
 
-       // load the-pit template
-      context.partial('../../templates/partials/the-pit.template');
+  // load .the-pit template and append to body
+  // then call api with blog post
+  context
+    .load('../../templates/partials/the-pit.template')
+    .then(e => (
+      $(e).appendTo(context.$element()),
+      blogPostsAjaxCall()
+    ));
 
-      // render the footer and header
-      context.render('../../templates/ui/header.template').prependTo('#main');
-     
 
-      // for each post fetched from the api
-      $.each(posts, (i, post) => {
-        // format post content
-        let content = trimContent(post.content, 350),
-            publishDate = formatDateForPost(post.publishDate),
-            updatedDate = post.updatedDate && formatDateForPost(post.updatedDate);
+  const blogPostsAjaxCall = () => {
+    $.ajax({
+      url: BLOG_ENDPOINT,
+      dataType: 'json',
+      success: posts => {
+
+        // for each post fetched from the api
+        $.each(posts, (i, post) => {
+          // format post content
+          let content = trimContent(post.content, 350),
+              publishDate = formatDateForPost(post.publishDate),
+              updatedDate = post.updatedDate && formatDateForPost(post.updatedDate);
+          
+          let updatedPost = Object.assign({}, post, { publishDate, updatedDate, content })
+          
+          // interpolate the variables on the template
+          // and append to '.blog-posts' in submit-form.template
+          // previously loaded into the context 
+          context
+            .render('../../templates/partials/posts.template', { post: updatedPost })
+            .appendTo('.the-pit');
+        });
         
-        let updatedPost = Object.assign({}, post, {publishDate, updatedDate, content})
-        
-        // interpolate the variables on the template
-        // and append to '.blog-posts' in submit-form.template
-        // previously loaded into the context 
-        context
-          .render('../../templates/partials/posts.template', { post: updatedPost })
-          .appendTo('.the-pit');
-      });
-
-      // render the footer
-      context.render('../../templates/ui/footer.template').appendTo('#main');
-      
-    },
-    error: err => handleApiError(err, context)
-  });
+      },
+      error: err => handleApiError(err, context)
+    });
+  };
 };
