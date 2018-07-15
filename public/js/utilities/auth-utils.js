4fb7b01ba4d15e01b5cf55e194973d8179ed0489
naxus28 <gabrielferraz27@gmail.com>
2018-07-15 00:14:26 -0400

    simplifies code on auth util

--- public/js/utilities/auth-utils.js
+++ public/js/utilities/auth-utils.js
@@ -1,7 +1,5 @@
 const isAuthenticated = context => {
-	const storedJwt  = context.app.session('jwt');
-	if (!storedJwt) return false;
-
-	const jwt = storedJwt && jwt_decode(context.app.session('jwt'));
-	return jwt.exp > moment().unix();
+	return !context.app.session('jwt') 
+		? false 
+		: jwt_decode(context.app.session('jwt')).exp > moment().unix();
 };
