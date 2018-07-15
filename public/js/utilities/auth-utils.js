13159da36ce5d569c0d1d529c901ee45c279cff8
naxus28 <gabrielferraz27@gmail.com>
2018-07-15 00:12:33 -0400

    fixes bug on auth util

--- public/js/utilities/auth-utils.js
+++ public/js/utilities/auth-utils.js
@@ -1,5 +1,6 @@
 const isAuthenticated = context => {
-	if (!context.app.session('jwt')) return false;
+	const storedJwt  = context.app.session('jwt');
+	if (!storedJwt) return false;
 
 	const jwt = storedJwt && jwt_decode(context.app.session('jwt'));
 	return jwt.exp > moment().unix();
